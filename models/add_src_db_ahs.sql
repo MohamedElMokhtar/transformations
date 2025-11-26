c55f84f0baea5e33b5525570a1831b5826ef2739
MohamedElMokhtar <deroua.mohamed.mokhtar@gmail.com>
2025-11-26 14:49:50 +0100

    changed materialized to incremental

--- models/add_src_db_ahs.sql
+++ models/add_src_db_ahs.sql
@@ -1,4 +1,10 @@
+{{ config(materialized='incremental', unique_key='id') }}
+
 select
     *,
     'AHS' as ABH
 from {{ source('public', 'ahs_factures') }}
+
+{% if is_incremental() %}
+where id not in (select id from {{ this }})
+{% endif %}
--- models/add_src_db_csm.sql
+++ models/add_src_db_csm.sql
@@ -1,4 +1,10 @@
+{{ config(materialized='incremental', unique_key='id') }}
+
 select
     *,
     'CSM' as ABH
 from {{ source('public', 'csm_factures') }}
+
+{% if is_incremental() %}
+where id not in (select id from {{ this }})
+{% endif %}
